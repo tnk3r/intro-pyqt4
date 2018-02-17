10e2313cab56cb3d9c75a3a434e4df47d858538d
tnk3r <abcfilm@gmail.com>
2018-02-17 14:53:15 -0800

    Added Pyqt4 Documentation

--- example1/example1.py
+++ example1/example1.py
@@ -2,12 +2,14 @@
 
 import sys
 from PyQt4.QtGui import QApplication, QMainWindow
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+# https://web.archive.org/web/20170816222409/http://pyqt.sourceforge.net/Docs/PyQt4/classes.html
 
 class example1(QMainWindow):
 
     def __init__(self):
         QMainWindow.__init__(self)
-        # self.setFixedSize(400, 400)
+        self.setFixedSize(400, 400)
 
         # self.setGeometry(0, 0, 300, 300)
         # self.setGeometry(300, 300, 400, 400)
@@ -16,12 +18,12 @@ class example1(QMainWindow):
         # self.setFixedHeight(800)
 
         self.show()
+
         self.raise_()
 
 
 app = QApplication(sys.argv)
+
 window = example1()
+
 sys.exit(app.exec_())
-
-
-
